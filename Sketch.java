30dec062f093efc56f981ddf4c25fc1f3bbf62b0
Andrew <andrew.chan23@ycdsbk12.ca>
2022-04-10 18:05:54 -0400

    Changed return method

--- Sketch.java
+++ Sketch.java
@@ -29,7 +29,7 @@ public class Sketch extends PApplet {
   public void draw() {
 
     // draw ground and grass
-    drawGround(0, 50);
+    drawGround(0, 50, 75);
     
     // draw house
     drawHouse(165, 325, 168, 97, 87, 124, 117, 110, 122, 184, 221);
@@ -38,7 +38,7 @@ public class Sketch extends PApplet {
     drawSun(100, 100, 75);
 
     // draw door
-    drawDoor(350, 500, 110, 150, 123, 68, 29);
+    drawDoor(400, 500, 110, 150, 123, 68, 29);
 
   }
   
@@ -48,10 +48,10 @@ public class Sketch extends PApplet {
    * Draws ground, grass, and grass blocks
    * 
    * @param fltStartingX: Starting X coordinate of grass block
-   * @param fltGrassWidth: width of each grass block
+   * @param fltGrassWidth: Width of each grass block
    * @author: A. Chan
    */
-  public void drawGround(float fltStartingX, float fltGrassWidth) {
+  public void drawGround(float fltStartingX, float fltGrassWidth, float fltGrassHeight) {
 
     noStroke();
 
@@ -62,7 +62,7 @@ public class Sketch extends PApplet {
     fill(147, 192, 97);
     for (float grassBlockX = fltStartingX; grassBlockX <= 800; grassBlockX+=(width/5)) {
 
-      rect(grassBlockX, 650, fltGrassWidth, 75);
+      rect(grassBlockX, 650, fltGrassWidth, fltGrassHeight);
 
     }
     
@@ -77,75 +77,74 @@ public class Sketch extends PApplet {
   /**
    * Draws house using coordinate and colour parameters
    * 
-   * @param houseX: X coordinate of composite house
-   * @param houseY: Y coordinate of house
-   * @param houseR: 'Red' colour of house (RGB)
-   * @param houseG: 'Green' colour of house (RGB)
-   * @param houseB: 'Blue' colour of house (RGB)
-   * @param triangleR: 'Red' colour of roof (RGB)
-   * @param triangleG: 'Green' colour of roof (RGB)
-   * @param triangleB: 'Blue' colour of roof (RGB)
-   * @param windowR: 'Red' colour of window (RGB)
-   * @param windowG: 'Green' colour of window (RGB)
-   * @param windowB: 'Blue' colour of window (RGB)
+   * @param fltHouseX: X coordinate of composite house
+   * @param fltHouseY: Y coordinate of house
+   * @param fltHouseR: 'Red' colour of house (RGB)
+   * @param fltHouseG: 'Green' colour of house (RGB)
+   * @param fltHouseB: 'Blue' colour of house (RGB)
+   * @param fltRoofR: 'Red' colour of roof (RGB)
+   * @param fltRoofG: 'Green' colour of roof (RGB)
+   * @param fltRoofB: 'Blue' colour of roof (RGB)
+   * @param fltWindowR: 'Red' colour of window (RGB)
+   * @param fltWindowG: 'Green' colour of window (RGB)
+   * @param fltWindowB: 'Blue' colour of window (RGB)
    * @author: A. Chan
    */
-  public void drawHouse(float houseX, float houseY, float houseR, float houseG, float houseB, float triangleR, float triangleG, float triangleB, float windowR, float windowG, float windowB) {
+  public void drawHouse(float fltHouseX, float fltHouseY, float fltHouseR, float fltHouseG, float fltHouseB, float fltRoofR, float fltRoofG, float fltRoofB, float fltWindowR, float fltWindowG, float fltWindowB) {
 
     // draw house
-    fill(houseR, houseG, houseB);
-    rect(houseX + 60, houseY, 350, 325);
+    fill(fltHouseR, fltHouseG, fltHouseB);
+    rect(fltHouseX + 60, fltHouseY, 350, 325);
 
     // draw roof
-    fill(triangleR, triangleG, triangleB);
-    triangle(houseX + 235, houseY - 150, houseX, houseY, houseX + 470, houseY);
+    fill(fltRoofR, fltRoofG, fltRoofB);
+    triangle(fltHouseX + 235, fltHouseY - 150, fltHouseX, fltHouseY, fltHouseX + 470, fltHouseY);
 
     // draw left window
-    fill(windowR, windowG, windowB);
-    ellipse(houseX + 148, houseY + 95, 50, 75);
-    line(houseX + 123, houseY + 95, houseX + 173, houseY + 95);
-    line(houseX + 148, houseY + 58, houseX + 148, houseY + 132);
+    fill(fltWindowR, fltWindowG, fltWindowB);
+    ellipse(fltHouseX + 148, fltHouseY + 95, 50, 75);
+    line(fltHouseX + 123, fltHouseY + 95, fltHouseX + 173, fltHouseY + 95);
+    line(fltHouseX + 148, fltHouseY + 58, fltHouseX + 148, fltHouseY + 132);
 
     // draw right window
-    ellipse(houseX + 323, houseY + 95, 50, 75);
-    line(houseX + 299, houseY + 95, houseX + 348, houseY + 95);
-    line(houseX + 323, houseY + 58, houseX + 323, houseY + 131);
+    ellipse(fltHouseX + 323, fltHouseY + 95, 50, 75);
+    line(fltHouseX + 299, fltHouseY + 95, fltHouseX + 348, fltHouseY + 95);
+    line(fltHouseX + 323, fltHouseY + 58, fltHouseX + 323, fltHouseY + 131);
 
   }
 
   /**
    * Draws the door and door knob of the house using coordinates, size, and colour parameters
    * 
-   * @param doorX: X coordinate of door
-   * @param doorY: Y coordinte of door
-   * @param doorWidth: Width of door
-   * @param doorHeight: Height of door
+   * @param fltDoorX: X coordinate of door
+   * @param fltDoorY: Y coordinte of door
+   * @param fltDoorWidth: Width of door
+   * @param fltDoorHeight: Height of door
    * @author: A. Chan
    */
-  public void drawDoor(float doorX, float doorY, float doorWidth, float doorHeight, float doorR, float doorG, float doorB) {
+  public void drawDoor(float fltDoorX, float fltDoorY, float fltDoorWidth, float fltDoorHeight, float fltDoorR, float fltDoorG, float fltDoorB) {
+
+    // draw door from the middle of screen
+    fltDoorX = centreDoorX(fltDoorX, fltDoorWidth);
 
     // draw door
-    fill(doorR, doorG, doorB);
-    rect(doorX, doorY, doorWidth, doorHeight);
+    fill(fltDoorR, fltDoorG, fltDoorB);
+    rect(fltDoorX, fltDoorY, fltDoorWidth, fltDoorHeight);
 
     // draw door knob
     fill(70, 71, 77);
-    ellipse(doorX + 25, doorY + 65, doorWidth * 0.15f, doorHeight * 0.1f);
+    ellipse(fltDoorX + 25, fltDoorY + 65, fltDoorWidth * 0.15f, fltDoorHeight * 0.1f);
 
   }
 
   /**
-   * Draws sun in the top-middle of the screen
+   * Given parameters of x, y, and size, draw sun
    * 
    * @param fltSunX: X coordinate of the sun
    * @param fltSunY: Y coordinate of the sun
    * @author: A. Chan
    */
   public void drawSun(float fltSunX, float fltSunY, float fltSize) {
-
-    // centre the sun
-    fltSunX = sunMiddleX(fltSunX);
-    fltSunY = sunMiddleY(fltSunY);
 
     // draw sun
     fill(255, 255, 35);
@@ -158,30 +157,21 @@ public class Sketch extends PApplet {
     line(fltSunX + 27, fltSunY + 27, fltSunX + 60, fltSunY + 60);
     line(fltSunX - 27, fltSunY + 25, fltSunX - 60, fltSunY + 60);
 
-
   }
   
   /**
-   * Given x float value, return x coordinate in the middle of the screen
+   * Draw door from the centre of the rectangle given X coordinate and width of door
    * 
-   * @param middleX: Uses x coordinate of sun from the drawSun method
-   * @return: Returns the x coordinate at the middle of the screen
+   * @param fltDoorX: X coordinate of the door
+   * @param fltWidth: Width of the door
+   * @return: Returns the centre X coordinate of the door
    * @author: A. Chan
    */
-  public float sunMiddleX(float middleX) {
-    return width/2;
+  public float centreDoorX(float fltDoorX, float fltWidth) {
+
+    return fltDoorX - fltWidth/2;
   }
 
-  /**
-   * Given y float value, return y coordinate at 1/15 of the screen
-   * 
-   * @param middleY: Uses y coordinte of sun from the drawSun method
-   * @return: Returns the y coordinate at 1/15 of the screen
-   * @author: A. Chan
-   */
-  public float sunMiddleY(float middleY) {
-    return height/15;
-  }
   
 
 }
